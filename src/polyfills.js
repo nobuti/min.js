6bcf99c1cb40f7b47347e2f14ee39a99b90c5cc5
nobuti <buti@nobuti.com>
2015-06-03 16:22:41 +0200

    Wip. Foreach polyfill.

--- src/polyfills.js
+++ src/polyfills.js
@@ -2,6 +2,44 @@
 
   'use strict';
 
+  var forEachPolyfill = function() {
+    // Production steps of ECMA-262, Edition 5, 15.4.4.18
+    // Reference: http://es5.github.io/#x15.4.4.18
+    if (!Array.prototype.forEach) {
+
+      Array.prototype.forEach = function(callback, thisArg) {
+
+        var T, k;
+
+        if (this == null) {
+          throw new TypeError(' this is null or not defined');
+        }
+
+        var O = Object(this);
+        var len = O.length >>> 0;
+        if (typeof callback !== "function") {
+          throw new TypeError(callback + ' is not a function');
+        }
+
+        if (arguments.length > 1) {
+          T = thisArg;
+        }
+
+        k = 0;
+
+        while (k < len) {
+          var kValue;
+
+          if (k in O) {
+            kValue = O[k];
+            callback.call(T, kValue, k, O);
+          }
+          k++;
+        }
+      };
+    }
+  }
+
   var querySelectorAllPolyfill = function() {
     if (!document.querySelectorAll) {
       document.querySelectorAll = function(selectors) {
@@ -218,6 +256,7 @@
     }
   }
 
+  forEachPolyfill();
   querySelectorAllPolyfill();
   eventListenerPolyfill();
   getComputedStylePolyfill();
